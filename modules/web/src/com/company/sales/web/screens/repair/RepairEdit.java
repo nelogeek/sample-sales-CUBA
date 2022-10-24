9bac250341ab472d4055a8eebc9238d930c713b7
nelogeek <lokotaevpr@gmail.com>
2022-10-24 17:19:56 +0500

    v4.4 - now when choosing a car service, the worker is erased

--- modules/web/src/com/company/sales/web/screens/repair/RepairEdit.java
+++ modules/web/src/com/company/sales/web/screens/repair/RepairEdit.java
@@ -1,11 +1,28 @@
 package com.company.sales.web.screens.repair;
 
+import com.haulmont.cuba.gui.model.DataContext;
+import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.sales.entity.Repair;
+
+import javax.inject.Inject;
 
 @UiController("sales_Repair.edit")
 @UiDescriptor("repair-edit.xml")
 @EditedEntityContainer("repairDc")
 @LoadDataBeforeShow
 public class RepairEdit extends StandardEditor<Repair> {
+
+    @Inject
+    private InstanceContainer<Repair> repairDc;
+
+    @Override
+    protected void onChangeEvent(DataContext.ChangeEvent event) {
+        repairDc.addItemPropertyChangeListener(e -> {
+            if (e.getProperty().equals("autoservice")) {
+
+                repairDc.getItem().setEmployee(null);
+            }
+        });
+    }
 }
